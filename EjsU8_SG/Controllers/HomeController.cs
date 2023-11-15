2fced7f6d9d3a52df2ebc8324f8a232bf63a5069
siu <francisco.rodriguez@iesnervion.es>
2023-11-15 14:28:15 +0100

    Agregar archivos de proyecto.

---
+++ EjsU8_SG/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using EjsU8_SG.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EjsU8_SG.Controllers
+{
+    public class HomeController : Controller
+    { 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Saludo()
+        {
+            return View();
+        }
+    }
+}
---
+++ EjsU8_SG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EjsU8_SG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
